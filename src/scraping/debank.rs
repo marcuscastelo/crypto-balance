dfae2529abadf156a9a0ff320e6b683cf34fa232
marcuscastelo <marcuscastelo@usp.br>
2024-05-11 12:30:15 -0300

    Update debank xpath after UI update

--- src/scraping/debank.rs
+++ src/scraping/debank.rs
@@ -6,7 +6,7 @@ impl DebankScraper {
     pub async fn get_total_balance(&self, user_id: &str) -> anyhow::Result<f64> {
         SimpleBalanceScrapper {
             url: format!("https://debank.com/profile/{}", user_id),
-            xpath: "//*[@id=\"root\"]/div[1]/div[2]/div[1]/div/div[2]/div/div[1]/div[2]/div[2]/div[1]/div[1]".to_owned(),
+            xpath: "//*[@id=\"root\"]/div[1]/div[1]/div/div/div/div[2]/div/div[1]/div[2]/div[2]/div[1]/div[1]".to_owned(),
             wait_time: 15,
         }.scrape().await
     }
