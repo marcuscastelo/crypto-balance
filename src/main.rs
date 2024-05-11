6c1b00a9e79f7ca191f88c2951800f725f880428
marcuscastelo <marcuscastelo@usp.br>
2024-05-11 11:41:20 -0300

    Update toolchain to nightly-2024-05-11

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-#![feature(once_cell)]
+#![feature(lazy_cell)]
 #![feature(async_closure)]
 
 mod app_config;
@@ -28,48 +28,4 @@ async fn main() {
 
     // Kill all geckodriver processes
     let _ = Command::new("pkill").arg("geckodriver").output();
-
-    return;
-
-    // Below: routine to get native token prices from CoinGecko (failed attempt)
-    let spreadsheet_manager = SpreadsheetManager::new(app_config::CONFIG.sheets.clone()).await;
-
-    let tokens = spreadsheet_manager
-        .read_range("'Prices per Chain - Airdrop Wallet'!B3:B1000")
-        .await
-        .unwrap()
-        .values
-        .map(|values| {
-            values
-                .iter()
-                .map(|value| {
-                    value[0]
-                        .clone()
-                        .to_string()
-                        .replace('\"', "")
-                        .to_uppercase()
-                })
-                .collect::<Vec<String>>()
-        })
-        .unwrap();
-
-    let coins = CoinGeckoApi.list_coins().await;
-    let mut coin_ids = coins
-        .into_iter()
-        .map(|coin| (coin.id, coin.symbol.to_uppercase()))
-        .collect::<Vec<_>>();
-    coin_ids.sort_by(|a, b| a.1.cmp(&b.1));
-
-    // let prices = CoinGeckoApi
-    //     .prices(coins.into_iter().map(|c| c.id).collect())
-    //     .await;
-
-    for coin_id in coin_ids {
-        if tokens.contains(&coin_id.1) {
-            println!("{:?}", coin_id);
-        }
-    }
-    // routines::UpdateAirdropWalletOnSheetsBalanceRoutine
-    //     .run()
-    //     .await
 }
